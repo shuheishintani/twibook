7614a5b6468466d88181e1b796bc49fb02e15b32
shuhei-cyf <shuhei.cyf@gmail.com>
2020-11-08 23:28:38 +0900

    firebase adminの環境変数設定

--- src/firebase/admin.js
+++ src/firebase/admin.js
@@ -4,7 +4,19 @@ import serviceAccount from './secret.json';
 
 if (firebaseAdmin.apps.length === 0) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount),
+    credential: firebaseAdmin.credential.cert({
+      type: process.env.FIREBASE_ADMIN_TYPE,
+      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+      token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+      auth_provider_x509_cert_url:
+        process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+    }),
     databaseURL: 'https://twibook-c1f70.firebaseio.com',
   });
 }
